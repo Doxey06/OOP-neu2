7309dce94a74ae95f9b18a97fd318086069582da
Dominik Krüger <119067638+Doxey06@users.noreply.github.com>
2025-08-04 19:52:48 +0200

    Compile sources before launching GUI

--- run-gui.bat
+++ run-gui.bat
@@ -1,3 +1,7 @@
 @echo off
+rem Compile sources so the latest GUI features are available
+javac --module-path javafx/lib --add-modules javafx.controls,javafx.fxml -cp ".;lib/sqlite-jdbc.jar;lib/slf4j-api.jar;lib/slf4j-simple.jar" -d . src\util\Database.java src\verwaltung\*.java src\model\*.java src\gui\*.java src\gui\views\*.java
+
+rem Start the application
 java --module-path javafx/lib --add-modules javafx.controls,javafx.fxml -cp ".;lib/sqlite-jdbc.jar;lib/slf4j-api.jar;lib/slf4j-simple.jar" gui.KlausurverwaltungGUI
 pause
